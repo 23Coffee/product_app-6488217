e781e3c26f13928b72418acf78e1bd58db3c1daf
Pattaradnai Kaeodumkoeng <122808660+23Coffee@users.noreply.github.com>
2024-01-31 11:57:58 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,32 +3,31 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
- // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '6488217 Pattaradnai Kaeodumkoeng',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        ),
-        home: MyHomePage(title: '6488217 Pattaradnai Kaeodumkoeng'
       ),
+      home: MyHomePage(title: '6488217 Pattaradnai Kaeodumkoeng'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key, required this.title}) 
-  : super(key: key);
+  MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("6488217 Product Listing",
-        style: TextStyle(color: Colors.white),
-      ),
-      backgroundColor: Colors.blue,
+        title: Text(
+          "6488217 Product Listing",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
       ),
       body: ListView(
         shrinkWrap: true,
@@ -36,48 +35,83 @@ class MyHomePage extends StatelessWidget {
         children: <Widget>[
           ProductBox(
             name: "iPhone",
-            description: "iPhone is the stylist phone ever",
+            description: "iPhone is the stylish phone ever",
             price: 1000,
-            image: "assets/iphone.jpg"),
+            image: 
+            "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-7inch-bluetitanium?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1692845699311",
+          ),
           ProductBox(
             name: "Pixel",
             description: "Pixel is the most featureful phone ever",
             price: 800,
-            image: "assets/pixel.jpg"),
+            image: "assets/pixel.jpg",
+          ),
           ProductBox(
             name: "Laptop",
             description: "Laptop is most productive development tool",
             price: 2000,
-            image: "assets/laptop.jpg"),
+            image:
+                'https://cdn.thewirecutter.com/wp-content/media/2023/06/laptops-2048px-5607.jpg?auto=webp&quality=75&crop=1.91:1&width=1200',
+          ),
           ProductBox(
             name: "Tablet",
             description: "Tablet is the most useful device ever for meeting",
             price: 1500,
-            image: "assets/tablet.jpg"),
+            image: "assets/tablet.jpg",
+          ),
           ProductBox(
             name: "Pendrive",
             description: "Pendrive is useful storage medium",
             price: 100,
-            image: "assets/pendrive.jpg"),
+            image: "assets/pendrive.jpg",
+          ),
           ProductBox(
             name: "Floppy Drive",
             description: "Floppy drive is useful rescue storage medium",
             price: 20,
-            image: "assets/floppy.jpg"),
+            image: 
+            "https://assets-global.website-files.com/621f7cd16d0599c647c9a299/64fcaac62f49bf12e33e2270_5.25-inch%20floppy%20disk%20AdobeStock_10670438.jpeg",
+          ),
         ],
-      )
+      ),
     );
- }
+  }
 }
 
 class ProductBox extends StatelessWidget {
-  ProductBox({ Key? key, required this.name, required this.description, required this.price, required this.image})
-    : super(key: key);
+  ProductBox(
+      {Key? key,
+      required this.name,
+      required this.description,
+      required this.price,
+      required this.image})
+      : super(key: key);
   final String name;
   final String description;
   final int price;
-  final String image;
+  final dynamic image;
+
+  @override
   Widget build(BuildContext context) {
+    Widget productImage;
+    if (image is String && (image as String).startsWith('http')) {
+      // Render network image if image is a URL
+      productImage = Image.network(
+        image,
+        height: 200,
+        width: 100,
+        fit: BoxFit.cover,
+      );
+    } else {
+      // Assume local asset if not a URL
+      productImage = Image.asset(
+        image,
+        height: 200,
+        width: 100,
+        fit: BoxFit.cover,
+      );
+    }
+
     return Container(
       padding: EdgeInsets.all(2),
       height: 120,
@@ -85,22 +119,26 @@ class ProductBox extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Image.asset(image,
-            height: 200,
-            width: 100,
-            ),
+            productImage,
             Expanded(
               child: Container(
                 padding: EdgeInsets.all(5),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
-                    Text(this.name,
-                      style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(
+                      this.name,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
                     Text(this.description),
                     Text("Price: " + this.price.toString()),
                   ],
-            )))
-      ])));
- }
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
